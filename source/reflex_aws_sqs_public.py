d850c6c2b862fed91e7116fc4967c4b7c5461aec
Chris Coleman <chris@rot-26.com>
2020-04-01 18:01:17 -0600

    extract and evaluate sqs policy statements

--- source/reflex_aws_sqs_public.py
+++ source/reflex_aws_sqs_public.py
@@ -5,24 +5,23 @@ import os
 
 import boto3
 from reflex_core import AWSRule
+from .policy_evaluator import PolicyEvaluator
 
 
 class SqsPublic(AWSRule):
-    """ TODO: A description for your rule """
+    """
+    Reflex Rule to detect publicly accessible SQS queues
+    """
 
-    # TODO: Instantiate whatever boto3 client you'll need, if any.
-    # Example:
-    # client = boto3.client("s3")
+    client = boto3.client("sqs")
 
     def __init__(self, event):
         super().__init__(event)
 
     def extract_event_data(self, event):
         """ Extract required event data """
-        # TODO: Extract any data you need from the triggering event.
-        #
-        # Example:
-        # self.bucket_name = event["detail"]["requestParameters"]["bucketName"]
+        self.queue_url = event["detail"]["requestParameters"]["queueUrl"]
+        self.queue_policy = self.get_queue_policy(self.queue_url)
 
     def resource_compliant(self):
         """
@@ -30,23 +29,28 @@ class SqsPublic(AWSRule):
 
         Return True if it is compliant, and False if it is not.
         """
-        # TODO: Implement a check for determining if the resource is compliant
+        policy_evaluator = PolicyEvaluator(self.queue_policy)
+        return policy_evaluator.policy_is_compliant()
 
     def remediate(self):
         """
         Fix the non-compliant resource so it conforms to the rule
         """
-        # TODO (Optional): Fix the non-compliant resource. This only needs to 
-        # be implemented for rules that remediate non-compliant resources.
-        # Purely detective rules can omit this function.
+        # TODO: This is possible to remove the policy statements by "Sid" with sqs.remove_permission()
+        pass
 
     def get_remediation_message(self):
         """ Returns a message about the remediation action that occurred """
-        # TODO: Provide a human readable message describing what occured. This
-        # message is sent in all notifications.
-        #
-        # Example:
-        # return f"The S3 bucket {self.bucket_name} was unencrypted. AES-256 encryption was enabled."
+        return f"The SQS Policy is too permissive. You are either using Principal: *, or you have no condition specified.\n Queue Url: {self.queue_url}"
+
+    def get_queue_policy(self, queue_url):
+        response = self.client.get_queue_attributes(
+            QueueUrl=queue_url,
+            AttributeNames=['Policy']
+        )
+        queue_policy = response['Attributes']['Policy']
+
+        return queue_policy
 
 
 def lambda_handler(event, _):
