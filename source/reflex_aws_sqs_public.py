8f2d737841091d5faf694b7da621249495a491d9
rjulian <richard@rjulian.net>
2020-09-21 00:29:55 -0700

    Fix for event payload parsing

--- source/reflex_aws_sqs_public.py
+++ source/reflex_aws_sqs_public.py
@@ -5,6 +5,7 @@ import os
 
 import boto3
 from reflex_core import AWSRule, subscription_confirmation
+
 # from .policy_evaluator import PolicyEvaluator
 
 
@@ -45,10 +46,9 @@ class SqsPublic(AWSRule):
 
     def get_queue_policy(self, queue_url):
         response = self.client.get_queue_attributes(
-            QueueUrl=queue_url,
-            AttributeNames=['Policy']
+            QueueUrl=queue_url, AttributeNames=["Policy"]
         )
-        queue_policy = response['Attributes']['Policy']
+        queue_policy = response["Attributes"]["Policy"]
 
         return queue_policy
 
@@ -56,15 +56,15 @@ class SqsPublic(AWSRule):
 def lambda_handler(event, _):
     """ Handles the incoming event """
     print(event)
-    if subscription_confirmation.is_subscription_confirmation(event):
-        subscription_confirmation.confirm_subscription(event)
+    event_payload = json.loads(event["Records"][0]["body"])
+    if subscription_confirmation.is_subscription_confirmation(event_payload):
+        subscription_confirmation.confirm_subscription(event_payload)
         return
-    rule = SqsPublic(json.loads(event["Records"][0]["body"]))
+    rule = SqsPublic(event_payload)
     rule.run_compliance_rule()
 
 
 class PolicyEvaluator(object):
-
     def __init__(self, policy_json):
         self.policy_json = policy_json
         self.get_policy_dict()
@@ -84,7 +84,7 @@ class PolicyEvaluator(object):
     @staticmethod
     def statement_has_condition(statement):
         condition = statement.get("Condition", {})
-        return bool( len(condition.keys()) > 0)
+        return bool(len(condition.keys()) > 0)
 
     def statement_is_compliant(self, statement):
         return bool(
